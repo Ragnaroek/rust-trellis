f0893b35475e0f378a9132e88361d1789f9f46b0
Michael Bohn <michael.bohn@freiheit.com>
2016-08-17 06:46:26 +0200

    added tests for write_display

--- src/mock.rs
+++ src/mock.rs
@@ -1,17 +1,26 @@
 use super::devices::I2CMasterDevice;
 use std::io::Result;
 
+pub type WriteCb = fn(i32, u8, &[u8]) -> ();
+
 pub struct MockDevice {
+    write_block_counter: i32,
+    write_block_cb: WriteCb
+
+    // TODO add write_block_cb: fn(i32) -> void and call counter to MockDevice
 }
 
 impl MockDevice {
-    pub fn new() -> MockDevice {
-        return MockDevice {};
+    pub fn new(write_block_cb: WriteCb) -> MockDevice {
+        return MockDevice {write_block_cb: write_block_cb, write_block_counter: 0};
     }
 }
 
 impl I2CMasterDevice for MockDevice {
-    fn write_block(&mut self, _register: u8, _values: &[u8]) -> Result<()> {
+    fn write_block(&mut self, register: u8, values: &[u8]) -> Result<()> {
+        let cb = self.write_block_cb;
+        cb(self.write_block_counter, register, values);
+        self.write_block_counter += 1;
         return Ok(());
     }
 }
--- tests/core.rs
+++ tests/core.rs
@@ -5,6 +5,9 @@ use trellis::core::Trellis;
 use trellis::core::Row;
 use trellis::core::Col;
 use trellis::mock::MockDevice;
+use trellis::mock::WriteCb;
+
+const FIRST_NON_INIT_CALL_ID:i32 = 4;
 
 #[test]
 fn should_set_led() {
@@ -31,17 +34,46 @@ fn should_clear_led() {
 }
 
 #[test]
+fn should_write_inital_display_data_to_i2c() {
+    fn write_cb(i:i32, reg: u8, values: &[u8]) {
+        if i >= FIRST_NON_INIT_CALL_ID {
+            let expected_vals:[u8; 16] = [0; 16];
+            assert_eq!(i, FIRST_NON_INIT_CALL_ID);
+            assert_eq!(reg, 0);
+            assert_eq!(values, expected_vals);
+        }
+    }
+    let mut trellis = new_trellis_with_cb(write_cb);
+    trellis.write_display();
+}
+
+#[test]
 fn should_write_display_data_to_i2c() {
-    // TODO
+    fn write_cb(i:i32, reg: u8, values: &[u8]) {
+        if i >= FIRST_NON_INIT_CALL_ID {
+            let expected_vals:[u8; 16] = [4, 0, 2, 0, 0, 2, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0];
+            assert_eq!(i, FIRST_NON_INIT_CALL_ID);
+            assert_eq!(reg, 0);
+            assert_eq!(values, expected_vals);
+        }
+    }
+    let mut trellis = new_trellis_with_cb(write_cb);
+    trellis.set_led(Col::A, Row::R0);
+    trellis.set_led(Col::B, Row::R1);
+    trellis.set_led(Col::C, Row::R2);
+    trellis.set_led(Col::D, Row::R3);
+    trellis.write_display();
 }
 
 // helper
 
-/**
- *
- */
 fn new_trellis() -> Trellis {
-    let mock_dev = MockDevice::new();
+    fn noop(_i:i32, _reg:u8, _vals: &[u8]) {};
+    return new_trellis_with_cb(noop);
+}
+
+fn new_trellis_with_cb(cb: WriteCb) -> Trellis {
+    let mock_dev = MockDevice::new(cb);
     let mut trellis = Trellis::new(Box::new(mock_dev));
     trellis.init();
     return trellis;
