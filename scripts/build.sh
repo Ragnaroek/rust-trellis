b28e9706cc22390d6d57cf0cc7d9439184d90b39
Michael Bohn <michael.bohn@freiheit.com>
2016-09-30 15:02:36 +0200

    updated build script to new rust version and docker image

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,1 @@
-# cross-compiles with the rust cross-compile docker image https://github.com/Ogeon/rust-on-raspberry-pi
-docker run --volume ~/pprojects/rust-trellis:/home/cross/project rust-stable-raspberry:1.11.0 build
+docker run --volume ~/pprojects/rust-trellis:/home/cross/project ragnaroek/rust-raspberry:1.12.0 build
