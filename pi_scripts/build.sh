cfb756083671f03d2e400b79cd069be8174814a3
Michael Bohn <michael.bohn@freiheit.com>
2016-11-17 06:39:35 +0100

    updated pi scripts

---
+++ pi_scripts/build.sh
@@ -0,0 +1,1 @@
+docker run --volume ~/pprojects/rust-trellis:/home/cross/project ragnaroek/rust-raspberry:1.12.1 build
--- scripts/build.sh
+++
@@ -1,1 +0,0 @@
-docker run --volume ~/pprojects/rust-trellis:/home/cross/project ragnaroek/rust-raspberry:1.12.0 build
